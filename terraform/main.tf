17933e12159f0de1a2677d6aa022f26c12501710
Jannetment360 <111163344+Jannetment360@users.noreply.github.com>
2022-09-11 22:57:06 +1000

    removed clous storage resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,25 +30,7 @@ provider "google" {
   region  = var.gcp_region
   project = var.gcp_project
 }
-resource "google_storage_bucket" "private-equity" {
-  name          = "tf-exp-project-362208"
-  location      = "australia-southeast1"
-  force_destroy = true
-    }
-resource "google_service_account" "service_account" {
-  account_id   = "tfexp-sa"
-  display_name = "TFEXP Service Account"
-}
 
-resource "google_project_iam_binding" "sa_account_iam" {
-project = var.gcp_project
-count = length(var.rolesList)
-role =  var.rolesList[count.index]
-members = [
-  "serviceAccount:${google_service_account.service_account.email}"
-]
-depends_on = [google_service_account.service_account]
-}
 # CloudSQL MySQL instance creation point-in-time-recovery.
 resource "google_sql_database_instance" "liveability-mysql" {
   project          = "tf-exp-project-362208"
